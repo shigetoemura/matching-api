a0b1e17e4ea9bc38893d242c8832802f8bb571b8
shigetoemura <shigeshac9@gmail.com>
2019-11-22 17:10:24 +0900

    change notification model

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::NotificationsController < Api::V1::ApplicationController
     before_action :authorize!
 
     def index
-        notifications = current_user.notifications.page(params[:page]).per(params[:limit]).order(updated_at: :desc)
-
-        from_user = User.where(id: notificatins.map(&:from_user_id))
-
-        render json: notifications, each_serializer: Api::V1::NotificationSerializer, from_user: from_user
-
+    	notifications = Notification.where(to_user_id: current_user.id).page(params[:page]).per(params[:limit]).order(updated_at: :desc)
+        serializer = ActiveModel::Serializer::CollectionSerializer.new(
+        	notifications,
+        	serializer: Api::V1::NotificationSerializer
+        	)
+        render json: serializer.as_json
     end
 end
