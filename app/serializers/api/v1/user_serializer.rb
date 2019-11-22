9cc8744ccaa3d25423941b0760fd5974c6c58557
shigetoemura <shigeshac9@gmail.com>
2019-11-22 15:44:35 +0900

    add favorite to user_serializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -9,6 +9,7 @@ class Api::V1::UserSerializer < ActiveModel::Serializer
   			:income,
   			:height,
   			:bio,
+  			:is_favorite,
   			:created_at,
   			:updated_at
 
@@ -19,4 +20,12 @@ class Api::V1::UserSerializer < ActiveModel::Serializer
   	def updated_at
   		object.updated_at.to_i
   	end
+
+  	def is_favorite
+      if instance_options[:current_user].present?
+        instance_options[:current_user].favorites.where(to_user_id: object).present?
+      else
+        false
+      end
+    end
 end
