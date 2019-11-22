359a6643b191dc504f2562b540d4b29bff23728a
shigetoemura <shigeshac9@gmail.com>
2019-11-22 16:24:57 +0900

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,11 @@ Rails.application.routes.draw do
 					post :skip
 				end
 			end
+			resources :messages, only: [:show] do
+				member do
+					resource :favorite, only: [:create, :destroy]
+				end
+			end
 		end
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
