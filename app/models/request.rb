dd7f3fb37e0b425815c75e72abf8289955f072e8
shigetoemura <shigeshac9@gmail.com>
2019-11-22 16:56:52 +0900

    change notification request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -5,4 +5,6 @@ class Request < ApplicationRecord
 
     belongs_to :user, foreign_key: "from_user_id"
     belongs_to :user, foreign_key: "to_user_id"
+
+    has_many :notifications, as: :notificable
 end
