5ba6e8b8d35e3264bf94f5d0af1b39233c6d4c42
shigetoemura <shigeshac9@gmail.com>
2019-11-22 15:31:25 +0900

    change def women

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -37,4 +37,18 @@ class Api::V1::UsersController < Api::V1::ApplicationController
 			render json: { message: "error" },status: 400
 		end
 	end
+
+	def women
+		if current_user.id = User.find(gender: 1)
+			query = User.where(gender: 1).page(params[:page]).per(params[:limit]).order(updated_at: :desc)
+	        serializer = ActiveModel::Serializer::CollectionSerializer.new(
+				query,
+				serializer: Api::V1::UserSerializer,
+				current_user: current_user
+			)
+			render json: serializer.as_json
+		else
+			render json: { message: "error" },status: 400
+		end
+	end
 end
