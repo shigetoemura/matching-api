d7ba352497c39f3f9267c100a7ad115170aec222
shigetoemura <shigeshac9@gmail.com>
2019-11-22 16:15:20 +0900

    create def update

--- app/controllers/api/v1/mes_controller.rb
+++ app/controllers/api/v1/mes_controller.rb
@@ -5,4 +5,47 @@ class Api::V1::MesController < Api::V1::ApplicationController
 		serializer = Api::V1::MeSerializer.new(current_user)
 		render json: serializer.as_json
 	end
+
+	def update
+		@user = current_user
+		if params[:user_params][:name].present?
+			@user.name = params[:user_params][:name]
+		end
+		if params[:user_params][:origin].present?
+			@user.origin = params[:user_params][:origin]
+		end
+		if params[:user_params][:job].present?
+			@user.job = params[:user_params][:job]
+		end
+		if params[:user_params][:intro].present?
+			@user.intro = params[:user_params][:intro]
+		end
+		if params[:user_params][:height].present?
+			@user.height = params[:user_params][:height]
+		end
+		if params[:user_params][:income].present?
+			@user.income = params[:user_params][:income]
+		end
+
+		if @user.save
+			serializer = Api::V1::MeSerializer.new(@user)
+			render json: serializer.as_json
+		else
+			raise ActiveRecord::RecordNotSaved
+		end
+
+  	end
+
+  	private
+
+  	def user_params
+    	params.require(:user_params).permit(
+    		:name,
+    		:origin,
+    		:job,
+    		:intro,
+    		:height,
+    		:income)
+  	end
+
 end
