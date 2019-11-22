518c4ee5c2870d632406152800171f5ac26ddcd9
shigetoemura <shigeshac9@gmail.com>
2019-11-22 15:05:59 +0900

    create sign_ups_controller

--- app/controllers/api/v1/sign_ups_controller.rb
+++ app/controllers/api/v1/sign_ups_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::SignUpsController < Api::V1::ApplicationController
+	protect_from_forgery :except => [:create]
+
+	def create
+		user = User.new(user_params)
+		user.save!
+		serializer = Api::V1::SignUpSerializer.new(user)
+		render json: serializer.as_json
+	end
+
+	private
+	def user_params
+		params.require(:user_params).permit(
+			:name,
+			:gender)
+	end
 end
