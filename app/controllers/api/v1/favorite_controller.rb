74b30ff25e9b3d855537cef50304a03099ff920d
shigetoemura <shigeshac9@gmail.com>
2019-11-22 15:48:04 +0900

    create def create

--- app/controllers/api/v1/favorite_controller.rb
+++ app/controllers/api/v1/favorite_controller.rb
@@ -1,2 +1,15 @@
-class Api::V1::FavoriteController < ApplicationController
+class Api::V1::FavoriteController < Api::V1::ApplicationController
+	before_action :authorize!
+	before_action :set_user
+
+	def create
+		Favorite.create!(favorable: @user, user_id: current_user.id, to_user_id: @user.id)
+		serializer = Api::V1::UserSerializer.new(@user, current_user: current_user)
+		render json: serializer.as_json
+	end
+
+	private
+	def set_user
+		@user = User.find_by!(id: params[:id])
+	end
 end
