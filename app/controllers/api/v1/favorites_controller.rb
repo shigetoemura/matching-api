89a26d0fd25873f5fa2ac9b3c558d335190e7fb7
shigetoemura <shigeshac9@gmail.com>
2019-11-22 16:05:12 +0900

    create def index

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::FavoritesController < Api::V1::ApplicationController
 	before_action :authorize!
-	before_action :set_user
+	before_action :set_user, only: [:create, :destroy]
 
 	def create
 		Favorite.create!(favorable: @user, user_id: current_user.id, to_user_id: @user.id)
@@ -14,6 +14,13 @@ class Api::V1::FavoritesController < Api::V1::ApplicationController
 		serializer = Api::V1::UserSerializer.new(@user, current_user: current_user)
 		render json: serializer.as_json
 	end
+
+	def index
+        favorites = current_user.favorites.page(params[:page]).per(params[:limit]).order(updated_at: :desc)
+        to_users = User.where(id: favorites.map(&:to_user_id))
+
+        render json: favorites, each_serializer: Api::V1::FavoriteSerializer, to_users: to_users
+    end
 
 	private
 	def set_user
