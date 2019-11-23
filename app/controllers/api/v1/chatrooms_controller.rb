f039aa3f25245569dd57099c9572ae7d44b817e5
shigetoemura <shigeshac9@gmail.com>
2019-11-23 14:13:58 +0900

    create chatroom

--- app/controllers/api/v1/chatrooms_controller.rb
+++ app/controllers/api/v1/chatrooms_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::ChatroomsController < Api::V1::ApplicationController
+	before_action :authorize!
+	before_action :set_user, only: [:create]
+
+	def create
+		ChatRoom.create!(user_id: current_user.id, opponent_user_id: @user.id)
+		render json: { message: "success" }
+	end
+
+	private
+	def set_user
+		@user = User.where(id: params[:id])
+	end
 end
