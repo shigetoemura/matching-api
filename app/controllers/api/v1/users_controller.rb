0944b982c38a306cadf5e58b7e93a20f38015129
shigetoemura <shigeshac9@gmail.com>
2019-11-26 19:11:58 +0900

    change def search

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,24 +3,49 @@ class Api::V1::UsersController < Api::V1::ApplicationController
 	before_action :set_user, except: [:search, :men, :women]
 
 	def search
-		query = User.where.not(id: current_user.id)
-		if params[:query].present?
-			query = query.where("origin like '%" + params[:query] + "%'")
-			.where("job like '%" + params[:query] + "%'")
-			.where("income like '%" + params[:query] + "%'")
-			.uniq
-			.page(params[:page])
-			.per(params[:limit])
-			.order(updated_at: :desc)
-		else
-			query = query.page(params[:page]).per(params[:limit]).order(updated_at: :desc)
-		end
-		serializer = ActiveModel::Serializer::CollectionSerializer.new(
-			query,
-			serializer: Api::V1::UserSerializer,
-			current_user: current_user
+		if 	current_user.gender == 2
+			query = User.where.not(id: current_user.id, gender: 2)
+			if params[:query].present?
+				query = query.where("origin like '%" + params[:query] + "%'")
+				.where("birthday like '%" + params[:query] + "%'")
+				.where("height like '%" + params[:query] + "%'")
+				.where("job like '%" + params[:query] + "%'")
+				.where("income like '%" + params[:query] + "%'")
+				.uniq
+				.page(params[:page])
+				.per(params[:limit])
+				.order(updated_at: :desc)
+			else
+				query = query.page(params[:page]).per(params[:limit]).order(updated_at: :desc)
+			end
+			serializer = ActiveModel::Serializer::CollectionSerializer.new(
+	        query,
+	        serializer: Api::V1::UserSerializer,
+	        current_user: current_user
 			)
-		render json: serializer.as_json
+			render json: serializer.as_json
+		else
+			query = User.where.not(id: current_user.id, gender: 1)
+			if params[:query].present?
+				query = query.where("origin like '%" + params[:query] + "%'")
+				.where("birthday like '%" + params[:query] + "%'")
+				.where("height like '%" + params[:query] + "%'")
+				.where("job like '%" + params[:query] + "%'")
+				.where("income like '%" + params[:query] + "%'")
+				.uniq
+				.page(params[:page])
+				.per(params[:limit])
+				.order(updated_at: :desc)
+			else
+				query = query.page(params[:page]).per(params[:limit]).order(updated_at: :desc)
+			end
+			serializer = ActiveModel::Serializer::CollectionSerializer.new(
+	        query,
+	        serializer: Api::V1::UserSerializer,
+	        current_user: current_user
+			)
+			render json: serializer.as_json
+		end
 	end
 
 	def men
