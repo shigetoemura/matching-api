2e9ff248ecb26cc6957d8e28960ee9d3c9b3ebfe
shigetoemura <shigeshac9@gmail.com>
2019-11-26 18:59:32 +0900

    create chatroom model

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,2 +1,5 @@
 class ChatRoom < ApplicationRecord
+	belongs_to :user
+	belongs_to :opponent_user, class_name: 'User'
+	has_many :messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 	has_many :chat_rooms, dependent: :destroy
 	has_many :favorites, dependent: :destroy
 	has_many :user_images, dependent: :destroy
+	has_many :messages
 	has_many :requests, foreign_key: "from_user_id", dependent: :destroy
     has_many :requests, foreign_key: "to_user_id", dependent: :destroy
 
